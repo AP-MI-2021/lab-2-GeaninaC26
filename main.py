48c6bb0811f37ff7168daca16f96b347e919ffec
Geanina <gianinaclipa@hmail.com>
2021-10-06 11:44:20 +0300

    Adaugare problema 3, lab 2

--- main.py
+++ main.py
@@ -48,10 +48,52 @@ def test_get_perfect_squares():
     assert get_perfect_squares(1, 10) == [1, 4, 9]
     assert get_perfect_squares(4, 20) == [4, 9, 16]
 
+def is_prime(nr):
+    '''
+    Testam daca numarul nr este prim
+    Input
+    -nr, numar natural, dat
+    Output
+    -True, daca nr prim, False daca nr compus 
+    '''
+    if nr<2 :
+        return False
+    if nr == 2 :
+        return True
+    for i in range (2, nr) :
+        if nr % i == 0 :
+            return False
+    return True
+        
+def is_superprime(nr) :
+    '''
+    Testam daca numarul nr este superprim
+    Input
+    -nr, numar natural, dat
+    Output
+    -True, daca nr superprim, False altfel 
+    '''
+    if nr == 0 :
+        return False
+    while nr > 0 :
+        if is_prime(nr) == False :
+            return False
+        nr = nr // 10
+    return True 
+
+def test_is_superprime() :
+    assert is_superprime(2) == True
+    assert is_superprime(252) == False
+    assert is_superprime(0) == False
+    assert is_superprime(230) == False
+    assert is_superprime(1) == False
+
+
 def main():
     while True:
         print('1.Numar palindrom.')
-        print('2.Patrate perfecte din intervalul [a,b]')
+        print('2.Patrate perfecte din intervalul [a,b].')
+        print('3.Numar Superprim')
         print('x.Exit');
         optiune=input('Alegeti o optiune:')
         if optiune == '1' :
@@ -66,6 +108,12 @@ def main():
                 b = int(input("Dati capatul de inchidere al intervalului: "))
                 test_get_perfect_squares()
                 print(get_perfect_squares(a, b))
+        elif optiune == '3' :
+                nr = int(input("Enter a number: "))
+                if is_superprime(nr) == True :
+                    print('Numarul este superprim.')
+                else :
+                    print('Numarul nu este superprim.')
         elif optiune == 'x' :
             break
         else :
